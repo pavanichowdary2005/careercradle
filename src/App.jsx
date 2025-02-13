36a2d4c642d020e7e6a6b868032961adf5618405
pavanichowdary2005 <pavanichowdhary@gmail.com>
2025-02-13 10:09:49 +0530

    changes have been made

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import Header from './components/Header';
-import Footer from './components/Footer';
 import About from './components/About';
 import Services from './components/Services';
 import Contact from './components/Contact';
@@ -8,13 +6,13 @@ import Contact from './components/Contact';
 const App = () => {
   return (
     <div>
-      <Header />
+      
       <main>
         <About />
         <Services />
         <Contact />
       </main>
-      <Footer />
+    
     </div>
   );
 };

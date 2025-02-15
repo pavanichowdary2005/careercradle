12c194e3a53f2fcea05d8c6c683c64e7418f1f65
pavanichowdary2005 <pavanichowdhary@gmail.com>
2025-02-15 11:17:40 +0530

    changes modified

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>Footer</p>
+            <div className="social-icons">
+                <a href="#">Facebook</a>
+                <a href="#">Twitter</a>
+                <a href="#">LinkedIn</a>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,22 @@
+import React from 'react';
+import './Header.css';
 
+const Header = () => {
+    return (
+        <header>
+            <h1>Home</h1>
+            <nav>
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <a href="#">Services</a>
+                <a href="#">Contact</a>
+            </nav>
+            <div className="auth">
+                <a href="#">Login</a>
+                <a href="#">Sign Up</a>
+            </div>
+        </header>
+    );
+}
+
+export default Header;

64d4463e616b9f775730f480bc5039c9aa7fa8dc
sreeja <gannamanisreeja@gmail.com>
2025-03-03 12:51:45 +0530

    register component handle submit fn is written with alert

--- src/components/pages/Register.jsx
+++ src/components/pages/Register.jsx
@@ -2,10 +2,15 @@ import React from "react";
 import "../Styles/Register.css";
 
 const Register = () => {
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert("Registered Successfully!");
+  }
   return (
     <div className="register-container">
       <h2>Register</h2>
-      <form>
+      <form onSubmit={handleSubmit}> 
         <input type="text" placeholder="Full Name" required />
         <input type="email" placeholder="Email" required />
         <input type="password" placeholder="Password" required />
